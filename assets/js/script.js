f1d55a29cce98b0f69fe7391e4f03699db5e88b4
Charles Enright <c.enright@me.com>
2024-03-11 14:28:17 +0000

    edit showFirstQuestion function to display question 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,25 +12,22 @@ function startGame() {
     // Hide the start button and the intro, and show the game container
     $("#start-btn, #intro").addClass("hidden");
     $("#game-container").removeClass("hidden");
-    showFirstQuestion();
+    showFirstQuestion(firstQuestion);
 }
 
 //Show Branch Question--------------------------------------------------------------------------
-function showFirstQuestion() {
-    const questionElement = document.getElementById('question-box');
-    questionElement.innerText = question[0].question;
+function showFirstQuestion(firstQuestion) {
+    $('#question-box').text(firstQuestion[0].question);
 }
 
 //QUESTIONS--------------------------------------------------------------------------
     // Branch Question
     const firstQuestion = [
         {
-            question: "When the city plunges into darkness, what stirs within you?",
+            question: "As the figure approaches, you see a glint of concern in their eyes. They offer you a hand and ask, 'Are you alright? You seem lost.' How do you respond?",
             answers: [
-                { text: "The desire to strike fear into the hearts of criminals"},
-                { text: "The determination to be a beacon of hope for the people"},
-                { text: "The urge to exploit chaos and seize power"},
-                { text: "The commitment to uphold justice and restore order"}
+                { text: "Accept their help gratefully, hoping they can shed light on your situation" },
+                { text: "Politely decline, feeling uneasy about trusting a stranger in this unfamiliar place" }
             ]
-        },
-    ]
+        }
+    ];
